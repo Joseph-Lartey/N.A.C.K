f643967797c7077a7306055d581af4c0a0a50e90
buzbunny <109228039+buzbunny@users.noreply.github.com>
2024-06-25 18:54:52 +0000

    add dependencies and signup pages

---
+++ frontend/lib/WelcomeScreen.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:untitled3/regScreen.dart';
+import 'loginScreen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/img3.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Gradient overlay
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Color.fromARGB(255, 183, 66, 91),
+                  Color.fromARGB(255, 183, 66, 91),
+                  Colors.transparent,
+                ],
+                stops: [0.0, 0.5, 1.0],
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent, // Transparent background
+              ),
+              child: ClipOval(
+                child: Image.asset(
+                  'assets/logo.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 200.0),
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                const SizedBox(height: 160),
+                const Text(
+                  'n.a.c.k',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 60,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Welcome to the NACK \n    Ashesi Dating App',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      _createRoute(const RegScreen()),
+                    );
+                  },
+                  child: Container(
+                    height: 53,
+                    width: 320,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(color: Colors.white),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'SIGN UP',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      _createRoute(const LoginScreen()),
+                    );
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0), // Add padding for better touch response
+                    child: const Text(
+                      "Don't have an account?  Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 19,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const SizedBox(height: 0),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Route _createRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset(0.0, 0.0);
+        const curve = Curves.ease;
+
+        final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+}
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'WelcomeScreen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text("NACK DEV")),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'inter',
+        useMaterial3: true,
+      ),
+      home: SplashScreen(), // SplashScreen as the home widget
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Delay navigation to WelcomeScreen after 3 seconds
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const WelcomeScreen()),
+      );
+    });
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              Color(0xFFB7425D), // Color at the bottom
+              Color(0xFF884244), // Color at the top
+            ],
+          ),
+        ),
+        // You can add your logo or any other content here
+        child: Center(
+          child: Center(
+            child: Image.asset('assets/logo.png'),
+          ),
+        ),
       ),
     );
   }
