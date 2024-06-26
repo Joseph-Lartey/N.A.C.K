c100c9c474f60d1be78c7cedcc10bc7cd736e85e
P-Asare <pinopalal11@gmail.com>
2024-06-26 18:54:32 +0000

    add user controller class

---
+++ backend/app/controllers/userController.php
@@ -0,0 +1,25 @@
+<?php
+
+    // Class to cater for all user oriented actions
+    class UserController {
+        protected $userModel;
+
+        public function __construct($pdo){
+            $this->userModel = new User($pdo);
+        }
+
+        // Hand create user action using the user model
+        public function createUser($data){
+            $this->userModel->createUser(
+                $data['firstname'],
+                $data['lastname'],
+                $data['username'],
+                $data['email'],
+                $data['password'],
+                $data['dob'],
+            );
+
+            return ['success' => true] ;
+        }
+    }
+?>
