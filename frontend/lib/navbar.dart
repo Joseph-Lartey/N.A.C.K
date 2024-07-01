2b6e87579a8211435a32f8bf3d7a796e0ab812d7
Palal <86783671+P-Asare@users.noreply.github.com>
2024-07-01 10:20:38 +0000

    added the profile page (#33)
    * added the profile page
    
    * made changes to the profile page
    
    ---------
    
    Co-authored-by: buzbunny <109228039+buzbunny@users.noreply.github.com>

--- frontend/lib/navbar.dart
+++ frontend/lib/navbar.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'profile.dart';
+import 'homePage.dart';
+//import 'settings.dart';
 
 class CustomBottomAppBar extends StatelessWidget {
   const CustomBottomAppBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
@@ -27,7 +31,7 @@ class CustomBottomAppBar extends StatelessWidget {
                       size: 30,
                     ),
                     onPressed: () {
-                      // Navigate to home screen or perform action
+                      _navigateTo(context, HomePage(), currentIndex: 0);
                     },
                   ),
                   IconButton(
@@ -47,7 +51,7 @@ class CustomBottomAppBar extends StatelessWidget {
                       size: 30,
                     ),
                     onPressed: () {
-                      // Navigate to profile screen or perform action
+                      _navigateTo(context, ProfilePage(), currentIndex: 2);
                     },
                   ),
                   IconButton(
@@ -57,7 +61,7 @@ class CustomBottomAppBar extends StatelessWidget {
                       size: 30,
                     ),
                     onPressed: () {
-                      // Navigate to settings screen or perform action
+                      //_navigateTo(context, SettingsPage(), currentIndex: 3);
                     },
                   ),
                 ],
@@ -65,6 +69,32 @@ class CustomBottomAppBar extends StatelessWidget {
             },
           ),
         ),
+      ),
+    );
+  }
+
+  void _navigateTo(BuildContext context, Widget page, {required int currentIndex}) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => page,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const Offset beginRight = Offset(1.0, 0.0);
+          const Offset endRight = Offset.zero;
+          const Offset beginLeft = Offset(-1.0, 0.0);
+          const Offset endLeft = Offset.zero;
+          const Curve curve = Curves.easeInOut;
+
+          final Animatable<Offset> tweenRight = Tween(begin: beginRight, end: endRight).chain(CurveTween(curve: curve));
+          final Animatable<Offset> tweenLeft = Tween(begin: beginLeft, end: endLeft).chain(CurveTween(curve: curve));
+
+          final Animation<Offset> offsetAnimation = animation.drive(currentIndex % 2 == 0 ? tweenLeft : tweenRight);
+
+          return SlideTransition(
+            position: offsetAnimation,
+            child: child,
+          );
+        },
       ),
     );
   }
