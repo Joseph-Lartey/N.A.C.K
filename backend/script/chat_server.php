0fdb7c03a7f8a5207d644dac4f689928b53fabba
Palal <86783671+P-Asare@users.noreply.github.com>
2024-07-07 00:40:14 +0000

    Socket chat server (#58)
    * install cboden/ratchet for socketing
    
    * create chat model
    
    * add chat controller class and functions
    
    * add get chat history route
    
    * create server file
    
    * add code to run socket server
    
    * add debug code
    
    * correct require to require once to fix error in chat

---
+++ backend/script/chat_server.php
@@ -0,0 +1,86 @@
+<?php
+
+use Ratchet\App;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+require_once __DIR__ . '/../config/database.php';
+
+class ChatServer implements MessageComponentInterface {
+
+    protected $clients;
+    protected $users;
+    protected $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+
+        $this->clients = new \SplObjectStorage;
+        $this->users = [];
+        $this->pdo = $pdo;
+    }
+
+    // Create a connection to be added to clients
+    public function onOpen(ConnectionInterface $conn)
+    {
+        // store the new connection
+        $this->clients->attach($conn);
+    }
+
+    // What happens when a message is sent through the connection
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        $data = json_decode($msg, true);
+
+        if(isset($data['userId'])){
+            $this->users[$data['userId']] = $from;
+        }
+
+        $fromUserId = $data['userId'];
+        $toUserId = $data['toUserId'];
+        $message = $data['message'];
+
+        // save the message to the database
+        $sql = "INSERT INTO messages (matchId, senderId, messageText) VALUES (:toUserId, :fromUserId, :messageSent)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['toUserId' => $toUserId, 'fromUserId' => $fromUserId, 'messageSent' => $message]);
+
+        if (isset($this->users[$toUserId])) {
+            $this->users[$toUserId]->send(json_encode([
+                'from_user_id' => $fromUserId,
+                'message' => $message,
+            ]));
+        }
+    }
+
+    // Handle the closing of a connection
+    public function onClose(ConnectionInterface $conn)
+    {
+        // Close the connection of a specific user
+        foreach ($this->users as $userId => $userConn) {
+            if ($userConn === $conn) {
+                unset($this->users[$userId]);
+                break;
+            }
+        }
+        $this->clients->detach($conn);
+    }
+
+    // Handle error pop ups
+    public function onError(ConnectionInterface $conn, Exception $e)
+    {
+        echo "error: {$e->getMessage()}";
+        $conn->close();
+    }
+
+}
+
+// Create database object
+$database = new Database();
+$pdo = $database->getPdo();
+
+// Run server
+$app = new App('localhost', 8080);
+$app->route('/chat', new ChatServer($pdo), ['*']);
+$app->run();
+
+?>
