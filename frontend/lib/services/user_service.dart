6f250ebc42f9f7913502f60bb337ef07b03f529c
Palal <86783671+P-Asare@users.noreply.github.com>
2024-07-24 05:22:17 +0000

    Add display all users and dislike users (#84)
    * create other user model
    
    * create user service and provider
    
    * add services and provider to display all users
    
    * add like and dislike function to homepage

---
+++ frontend/lib/services/user_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../models/other_user.dart';
+
+class UserService {
+  final String baseUrl = "http://16.171.150.101/N.A.C.K/backend";
+
+  // get all users in the database
+  Future<List<OtherUser>> getAllUsers() async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/users'));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+
+        if (data['success'] == true) {
+          List<dynamic> usersJson = data['data'];
+          List<OtherUser> users =
+              usersJson.map((json) => OtherUser.fromJson(json)).toList();
+          return users;
+        } else {
+          throw Exception('Error: ${data['error']}');
+        }
+      } else {
+        throw Exception('Failed to load users: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to load users: $e');
+    }
+  }
+}
