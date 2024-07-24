2df3af4c59925eb943c045ddd2b9587e2ee79d88
kwakuNti <159504184+kwakuNti@users.noreply.github.com>
2024-07-24 00:40:28 +0000

    updates to the home

--- frontend/lib/screens/profile.dart
+++ frontend/lib/screens/profile.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import '../widgets/navbar.dart';  
+import 'package:provider/provider.dart';
+import 'package:untitled3/providers/auth_provider.dart';
+import '../widgets/navbar.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    final user = authProvider.user;
+    final userProfileImage =
+        'http://16.171.150.101/N.A.C.K/backend/public/profile_images/${user?.profileImage ?? 'default_user.png'}';
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -12,29 +18,29 @@ class ProfilePage extends StatelessWidget {
             Container(
               color: const Color.fromARGB(255, 183, 66, 91),
               padding: const EdgeInsets.all(80),
-              child: const Column(
+              child: Column(
                 children: [
-                   CircleAvatar(
+                  CircleAvatar(
                     radius: 50,
-                    backgroundImage: AssetImage('assets/img2.jpg'),
+                    backgroundImage: NetworkImage(userProfileImage),
                   ),
-                   SizedBox(height: 10),
-                   Text(
-                    'Jhon Abraham',
-                    style: TextStyle(
+                  SizedBox(height: 10),
+                  Text(
+                    '${user?.firstName ?? ''} ${user?.lastName ?? ''}',
+                    style: const TextStyle(
                       color: Colors.white,
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                   Text(
-                    '@jhonabraham',
-                    style: TextStyle(
+                  Text(
+                    '@${user?.username ?? ''}',
+                    style: const TextStyle(
                       color: Colors.white70,
                       fontSize: 16,
                     ),
                   ),
-                   SizedBox(height: 20),
+                  SizedBox(height: 20),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                   ),
@@ -42,7 +48,8 @@ class ProfilePage extends StatelessWidget {
               ),
             ),
             Container(
-              color: const Color.fromARGB(255, 183, 66, 91), // Maintain red background
+              color: const Color.fromARGB(
+                  255, 183, 66, 91), // Maintain red background
               child: ClipRRect(
                 borderRadius: const BorderRadius.only(
                   topLeft: Radius.circular(40),
@@ -51,30 +58,30 @@ class ProfilePage extends StatelessWidget {
                 child: Container(
                   color: Colors.white,
                   padding: const EdgeInsets.all(16),
-                  child: const Column(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                       ProfileInfoRow(
+                      ProfileInfoRow(
                         label: 'First Name',
-                        value: 'Jhon',
+                        value: user?.firstName ?? '',
                       ),
-                       ProfileInfoRow(
+                      ProfileInfoRow(
                         label: 'Last Name',
-                        value: 'Abraham',
+                        value: user?.lastName ?? '',
                       ),
-                       ProfileInfoRow(
+                      ProfileInfoRow(
                         label: 'Username',
-                        value: 'Bomber',
+                        value: user?.username ?? '',
                       ),
-                       ProfileInfoRow(
+                      ProfileInfoRow(
                         label: 'Email Address',
-                        value: 'jhonabraham20@gmail.com',
+                        value: user?.email ?? '',
                       ),
-                       ProfileInfoRow(
+                      ProfileInfoRow(
                         label: 'Bio',
-                        value: 'Hey there! I\'m Alex, a 29-year-old musician who loves exploring new places and diving into a good book. When I\'m not strumming my guitar, you can find me hiking up a mountain or planning my next adventure. Looking for someone who enjoys great conversations, has a sense of humor, and is up for spontaneous road trips. Let\'s create some amazing memories together!',
+                        value: 'Hey there! I',
                       ),
-                       SizedBox(height: 20),
+                      SizedBox(height: 20),
                     ],
                   ),
                 ),
@@ -120,4 +127,3 @@ class ProfileInfoRow extends StatelessWidget {
     );
   }
 }
-
